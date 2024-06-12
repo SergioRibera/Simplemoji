d0ff652636abe63fad221250f92c4db97e7f521c
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-06-11 21:12:29 -0400

    refactor: remove custom hoverable component

--- src/components/emoji.rs
+++ src/components/emoji.rs
@@ -1,25 +1,22 @@
 use emojis::Emoji;
-use iced::widget::{button, Text};
+use iced::widget::{button, MouseArea, Text};
 use iced::Element;
 use iced_tiny_skia::core::text::Shaping;
 
 use crate::app::MainAppMessage;
 use crate::styles::get_btn_transparent_style;
 
-use super::Hoverable;
-
 pub fn render_emoji(e: &str) -> Element<'_, MainAppMessage> {
     Text::new(e).shaping(Shaping::Advanced).into()
 }
 
 pub fn render_emoji_btn(e: &Emoji) -> Element<'_, MainAppMessage> {
-    Hoverable::new(
+    MouseArea::new(
         button(render_emoji(e.as_str()))
             .style(get_btn_transparent_style(false))
             .on_press(MainAppMessage::CopyEmoji(e.to_string()))
-            .into(),
     )
-    .on_hover(MainAppMessage::HoverEmoji(
+    .on_enter(MainAppMessage::HoverEmoji(
         e.name().to_string(),
         e.to_string(),
         e.shortcodes().map(|s| s.to_string()).collect(),
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,7 +1,5 @@
 mod emoji;
-mod hoverable;
 mod tone_selector;
 
 pub use emoji::*;
-pub use hoverable::*;
 pub use tone_selector::*;
