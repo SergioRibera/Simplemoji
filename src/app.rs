8056d741afb8d81c11e16cb1b2e7ca924c88198a
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-07-02 23:57:33 -0400

    feat: show search conditional from args

--- src/app.rs
+++ src/app.rs
@@ -62,6 +62,7 @@ impl MainApp {
     pub fn set_globals(&self) {
         let global = self.window.global::<MainState>();
         global.set_show_preview(self.settings.show_preview);
+        global.set_show_search(self.settings.show_search);
         if let Some(font) = self.settings.font.as_deref() {
             global.set_font(SharedString::from(font));
         }
